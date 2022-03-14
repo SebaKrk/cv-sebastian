82ac2ca643429f8ab578fc6d58250ac8a53b4318
SebaKrk <s.sciuba@gmail.com>
2022-03-14 11:54:51 +0100

    Initial Commit

---
+++ SciubaSebastianCV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SciubaSebastianCV
+//
+//  Created by Sebastian Sciuba on 14/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
