81646908a9fa38d8e160025dc2b0d467eeb4cebc
Emilia Czyrak <s.sciuba@gmail.com>
2022-03-15 12:36:21 +0100

    added scrollView to  AbutMeView

--- SciubaSebastianCV/View/AboutMeView.swift
+++ SciubaSebastianCV/View/AboutMeView.swift
@@ -11,10 +11,18 @@ import UIKit
 class AbutMeView : UIViewController {
     
     let container = UIView()
+    let scrolView = UIScrollView()
+    let contenView = UIView()
+    let contentLabel = UILabel()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureContainer()
+        configureScrollView()
+        configureContenView()
+        configureContenLabel()
+        
     }
     
     private func configureContainer() {
@@ -22,14 +30,52 @@ class AbutMeView : UIViewController {
         container.translatesAutoresizingMaskIntoConstraints = false
         
         container.backgroundColor = .systemGray6
+        container.layer.cornerRadius = 5
         
         NSLayoutConstraint.activate([
             container.topAnchor.constraint(equalTo: view.topAnchor),
             container.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             container.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            container.heightAnchor.constraint(equalToConstant: 200)
+            container.heightAnchor.constraint(equalToConstant: 150)
+        ])
+    }
+    private func configureScrollView() {
+        container.addSubview(scrolView)
+        scrolView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            scrolView.topAnchor.constraint(equalTo: container.topAnchor),
+            scrolView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            scrolView.trailingAnchor.constraint(equalTo: container.trailingAnchor),
+            scrolView.bottomAnchor.constraint(equalTo: container.bottomAnchor)
         ])
     }
     
+    private func configureContenView() {
+        scrolView.addSubview(contenView)
+        contenView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contenView.topAnchor.constraint(equalTo: scrolView.topAnchor),
+            contenView.leadingAnchor.constraint(equalTo: scrolView.leadingAnchor,constant: 10),
+            contenView.trailingAnchor.constraint(equalTo: scrolView.trailingAnchor),
+            contenView.bottomAnchor.constraint(equalTo: scrolView.bottomAnchor)
+        ])
+    }
+    private func configureContenLabel() {
+        contenView.addSubview(contentLabel)
+        contentLabel.translatesAutoresizingMaskIntoConstraints = false
     
+        contentLabel.text = Constants.aboutMe
+        contentLabel.textColor = .black
+        contentLabel.textAlignment = .left
+        contentLabel.numberOfLines = 0
+        contentLabel.sizeToFit()
+    
+        NSLayoutConstraint.activate([
+            contentLabel.topAnchor.constraint(equalTo: contenView.topAnchor, constant: 10),
+            contentLabel.widthAnchor.constraint(equalTo: scrolView.widthAnchor, multiplier: 0.95),
+            contentLabel.bottomAnchor.constraint(equalTo: contenView.bottomAnchor)
+        ])
+    }
 }
