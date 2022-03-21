f4c9aadd55e5fba56cef9c55046531163fa819ae
Sebastian Sciuba <s.sciuba@gmail.com>
2022-03-21 14:13:54 +0100

    create Experience model

---
+++ SciubaSebastianCV/Model/Experience.swift
@@ -0,0 +1,25 @@
+//
+//  Experience.swift
+//  SciubaSebastianCV
+//
+//  Created by akra on 21/03/2022.
+//
+
+import Foundation
+
+struct Experience {
+    let company : String
+    let startDate : String
+    let endDate : String
+    let position : String
+    let location : String
+    let responsibilities : Responsibilities
+}
+
+struct Responsibilities {
+    let first : String
+    let second : String
+    let third : String
+}
+
+
