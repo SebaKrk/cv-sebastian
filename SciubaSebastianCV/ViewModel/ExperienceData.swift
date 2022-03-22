17b9babca3ba364f8601d63a7c1d5ce338877c1e
Sebastian Sciuba <s.sciuba@gmail.com>
2022-03-22 10:52:00 +0100

    clean up the code and added new folders

--- SciubaSebastianCV/ViewModel/ExperienceData.swift
+++ SciubaSebastianCV/ViewModel/ExperienceData.swift
@@ -7,23 +7,6 @@
 
 import Foundation
 
-//struct ExperienceData {
-//    let company1 = Experience(company: "Freelance", startDate: "2021-01", endDate: "present", position: "iOS Developer", location: "Krakow", responsibilities: Responsibilities(
-//        first: "Tworzenie aplikacji na platforme iOS",
-//        second: "Projektowanie aplikacji oraz zawansowanych intrfejsow",
-//        third: "Poszukiwanie nowych rozwiazan"))
-//    
-//    let company2 = Experience(company: "UTest", startDate: "2020-03", endDate: "2021-01", position: "Software Tester", location: "Krakow", responsibilities: Responsibilities(
-//        first: "Testowanie manualne aplikacji mobilnych i web",
-//        second: "Samodzielne prowadzenie analizy przyczyny powstanie bledow",
-//        third: "Tworzenie scenariuszy i przypadkow testowych"))
-//    
-//    let company3 = Experience(company: "Calypso Fitness", startDate: "2018-03", endDate: "present", position: "General Manager", location: "Krakow", responsibilities: Responsibilities(
-//        first: "Zarzadzanie codziennym funkcionwaniem klubu fitness",
-//        second: "Rekrutacja, zarzadzanie zespolem oraz placami",
-//        third: "Dbanie o zaopatrzenie klubu"))
-//}
-
 func getExpData() -> [Experience] {
     let company1 = Experience(company: "ARKA", startDate: "2022-03", endDate: "present", position: "Intern iOS Developer", location: "Krakow", responsibilities: Responsibilities(first: "Learning", second: "Learning", third: "Learning"))
     let company2 = Experience(company: "Freelance", startDate: "2021-01", endDate: "2022-02", position: "iOS Developer", location: "Krakow", responsibilities: Responsibilities(
