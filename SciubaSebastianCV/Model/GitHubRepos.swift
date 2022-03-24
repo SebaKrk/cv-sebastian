9597fe798844a4fb4511780087c38aa239312b06
Sebastian Sciuba <s.sciuba@gmail.com>
2022-03-24 12:24:25 +0100

    created GitHubRepos model

---
+++ SciubaSebastianCV/Model/GitHubRepos.swift
@@ -0,0 +1,20 @@
+//
+//  GitHubRepos.swift
+//  SciubaSebastianCV
+//
+//  Created by akra on 24/03/2022.
+//
+
+import Foundation
+
+struct GitHubRepos {
+    let name : String
+    let html_url : String
+    let description : String?
+    
+    let created_at: String
+    let updated_at: String
+    let pushed_at : String
+    
+    let language : String
+}
