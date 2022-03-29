0d34171510c7bdb57ff7d4470e026e150d2c652a
Sebastian Sciuba <s.sciuba@gmail.com>
2022-03-29 10:44:17 +0200

    remove GitHubReposViewModel

--- SciubaSebastianCV/ViewModel/GitHubReposViewModel.swift
+++ SciubaSebastianCV/ViewModel/GitHubReposViewModel.swift
@@ -5,27 +5,27 @@
 //  Created by akra on 28/03/2022.
 //
 
-import Foundation
-
-protocol GitHubReposViewModellDelegate : AnyObject {
-    func updateView(repos: [GitHubRepos])
-}
-
-class GitHubReposViewModel {
-    
-    var ghReposViewModelDelegate : GitHubReposViewModellDelegate?
-    
-    func getRepoData() {
-        Networking.shared.getReposData { result in
-            switch result {
-                
-            case .success( let repo):
-                DispatchQueue.main.async {
-                    self.ghReposViewModelDelegate?.updateView(repos: repo)
-                }
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-}
+//import Foundation
+//
+//protocol GitHubReposViewModellDelegate : AnyObject {
+//    func updateView(repos: [GitHubRepos])
+//}
+//
+//class GitHubReposViewModel {
+//    
+//    var ghReposViewModelDelegate : GitHubReposViewModellDelegate?
+//    
+//    func getRepoData() {
+//        Networking.shared.fetchReposData { result in
+//            switch result {
+//                
+//            case .success( let repo):
+//                DispatchQueue.main.async {
+//                    self.ghReposViewModelDelegate?.updateView(repos: repo)
+//                }
+//            case .failure(let error):
+//                print(error.localizedDescription)
+//            }
+//        }
+//    }
+//}
