b340f0a0ceec5286d631853e05c58c9339263436
Sebastian Sciuba <s.sciuba@gmail.com>
2022-04-01 11:57:51 +0200

    create Course struct and set constraints for skillsAndCoursesView

--- SciubaSebastianCV/Controller/SkillsAndCourses/SkillsAndCoursesVC.swift
+++ SciubaSebastianCV/Controller/SkillsAndCourses/SkillsAndCoursesVC.swift
@@ -9,8 +9,34 @@ import UIKit
 
 class SkillsAndCoursesVC : UIViewController {
     
+    let skillsAndCoursesView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemMint
+        
+        setupView()
+        configureView()
     }
+    
+    private func setupView() {
+        view.backgroundColor = .white
+        configureView()
+    }
+    
+//    MARK: - Constraints
+    
+    private func configureView() {
+        view.addSubview(skillsAndCoursesView)
+        skillsAndCoursesView.translatesAutoresizingMaskIntoConstraints = false
+        
+        skillsAndCoursesView.backgroundColor = .red
+        
+        NSLayoutConstraint.activate([
+            skillsAndCoursesView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            skillsAndCoursesView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            skillsAndCoursesView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            skillsAndCoursesView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
 }
---
+++ SciubaSebastianCV/Model/Courses.swift
@@ -0,0 +1,21 @@
+//
+//  Courses.swift
+//  SciubaSebastianCV
+//
+//  Created by akra on 01/04/2022.
+//
+
+import Foundation
+
+struct Courses {
+    let title : String
+    let author : String
+    let courseImg : String
+    let type : String
+    let skills : Skills
+}
+struct Skills {
+    let first : String
+    let second : String
+    let third : String
+}
